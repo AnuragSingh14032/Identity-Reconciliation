c2f54f8d45746d1d72ee3825180484ab955f4659
AnuragSingh14032 <anuragsingh14032@gmail.com>
2026-03-02 23:03:18 +0530

    Update controller

--- src/controllers/identify.controller.ts
+++ src/controllers/identify.controller.ts
@@ -5,12 +5,18 @@ export const identifyHandler = async (req: Request, res: Response) => {
   try {
     const { email, phoneNumber } = req.body;
 
+    if (!email && !phoneNumber) {
+      return res.status(400).json({
+        error: "At least one of email or phoneNumber is required",
+      });
+    }
+
     const result = await identifyService(email, phoneNumber);
 
     return res.status(200).json(result);
   } catch (error: any) {
-    return res.status(400).json({
-      error: error.message,
+    return res.status(500).json({
+      error: "Internal Server Error",
     });
   }
 };
